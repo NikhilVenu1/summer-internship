0eed7bc51713a88490d8a87c36a8d5946d78c4d8
Nikhil Venu <128724004+NikhilVenu1@users.noreply.github.com>
2023-05-08 15:03:47 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+var fruits= ["mango","orange","grape","banana"];
+console.log(fruits.length);
+fruits[4]="pineapple";
+fruits.push("jack fruit");
+fruits.pop();
+fruits.shift();
+fruits.unshift("guava");
+console.log(fruits);
---
+++ arrayobjects.js
@@ -0,0 +1,13 @@
+var person =[
+    {name:"NIK",
+    age:20,
+    place:"aluva",
+},
+{name:"ACJ",
+age:70,
+place:"koovapady",}
+
+]
+// console.log(person)
+console.log(`my name is ${person[0].name} lives in ${person[0].place}`);
+console.log(`my name is ${person[1].name} lives in ${person[1].place}`);
