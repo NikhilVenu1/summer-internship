8ad560bbd8ee1f55843b6fc2e04fbfa010305376
Nikhil Venu <128724004+NikhilVenu1@users.noreply.github.com>
2023-05-08 15:46:18 +0530

    Add files via upload

---
+++ arrayfun.js
@@ -0,0 +1,19 @@
+/* function add(a,b){
+    let c=a+b;
+    // console.log(c);
+}
+add(5,10);*/
+
+/* arrow functions
+ var add = (a,b)=>{
+
+    console.log(a+b);
+ }
+ add(2,2);*/
+ /*var mul = (a,b)=>{
+    console.log(a*b);
+
+ }
+ mul(3,2);*/
+
+
---
+++ loop.js
@@ -0,0 +1,30 @@
+/*
+for ( let i=1;i<=10;i++)
+ 
+console.log(i);
+*/
+/*
+let num =[2,4,6,10];
+let sum=0;
+for (let i=0;i<num.length;i++)
+{
+    sum= sum + num[i];
+}
+console.log(sum);
+*/
+/*
+let i=1;
+while(i<=10)
+{
+console.log(i);
+i++
+}
+*/
+
+let i=1;
+do{
+    console.log(i);
+    i++;
+
+}
+while(i<=10)
